0c32c87adcb5fd2ffa4b25ecf2a2eafc45e72eb5
reneeb <info@perl-services.de>
2019-02-11 07:38:46 +0100

    prepare release

--- lib/Mojolicious/Command/Author/generate/localplugin.pm
+++ lib/Mojolicious/Command/Author/generate/localplugin.pm
@@ -4,7 +4,7 @@ use Mojo::Base 'Mojolicious::Command';
 use Mojo::Util qw(camelize class_to_path decamelize);
 use Mojolicious;
 
-our $VERSION = '0.02';
+our $VERSION = 0.03;
 
 has description => 'Generate Mojolicious plugin directory structure for application';
 has usage => sub { shift->extract_usage };
